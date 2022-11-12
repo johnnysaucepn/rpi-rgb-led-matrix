b2542ade354416cbbe6f75dd9918d2857f9b5199
James Howat <james@thehowats.net>
2022-11-12 23:48:16 +0000

    Add a more complex hosted project.

---
+++ bindings/csharp-dotnet/DansClock/ClockFace.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using rpi_rgb_led_matrix_sharp;
+
+namespace DansClock
+{
+    public class ClockFace : IHostedService
+    {
+        private readonly RGBLedMatrixOptionsFactory _optionsFactory;
+        private readonly IConfiguration _config;
+
+        public ClockFace(RGBLedMatrixOptionsFactory optionsFactory, IConfiguration config)
+        {
+            _optionsFactory = optionsFactory;
+            _config = config;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            RGBLedMatrixOptions opts = _optionsFactory.Build();
+
+            var fontFile = "6x12.bdf";
+            var hexColor = _config.GetValue<string>("color");
+
+            var color = Color.HexToColor(hexColor, new Color(0, 255, 0));
+
+            var matrix = new RGBLedMatrix(opts);
+            var canvas = matrix.CreateOffscreenCanvas();
+            var font = new RGBLedFont(fontFile);
+            var glyphWidth = 6;
+            var glyphHeight = 12;
+
+            int y = (canvas.Height - glyphHeight) / 2;
+            int x = (canvas.Width - (8 * glyphWidth)) / 2;
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var currentTime = DateTime.Now.ToLongTimeString(); 
+                canvas.DrawText(font, 1, 6, color, currentTime);
+                matrix.SwapOnVsync(canvas);
+                await Task.Delay(TimeSpan.FromMilliseconds(500), cancellationToken);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ bindings/csharp-dotnet/DansClock/Program.cs
@@ -0,0 +1,11 @@
+﻿using DansClock;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<ClockFace>();
+        services.AddSingleton<RGBLedMatrixOptionsFactory>();
+    })
+    .RunConsoleAsync();

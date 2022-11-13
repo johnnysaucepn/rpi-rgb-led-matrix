78e280a22648e5ddf3a3c58588c69774e72b2b82
James Howat <james@thehowats.net>
2022-11-13 22:20:07 +0000

    Add background border.
    Tweak font position.

--- bindings/csharp-dotnet/DansClock/ClockFace.cs
+++ bindings/csharp-dotnet/DansClock/ClockFace.cs
@@ -25,26 +25,35 @@ namespace DansClock
             Console.WriteLine("Starting...");
             RGBLedMatrixOptions opts = _optionsFactory.Build();
 
-            var fontFile = "6x12.bdf";
-            var hexColor = _config.GetValue<string>("color");
+            var fontFile = "6x13O.bdf";
+            var hexTextColor = _config.GetValue<string>("color");
+            var hexBgColor = _config.GetValue<string>("bg");
 
-            var color = Color.HexToColor(hexColor, new Color(0, 255, 0));
+            var textColor = Color.HexToColor(hexTextColor, new Color(0, 128, 0));
+            var bgColor = Color.HexToColor(hexBgColor, new Color(128, 00, 0));
 
             var matrix = new RGBLedMatrix(opts);
             var canvas = matrix.CreateOffscreenCanvas();
             var font = new RGBLedFont(fontFile);
             var glyphWidth = 6;
-            var glyphHeight = 12;
+            var glyphHeight = 13;
 
-            int y = (canvas.Height - glyphHeight) / 2;
+            int y = (canvas.Height + glyphHeight) / 2;
             int x = (canvas.Width - (8 * glyphWidth)) / 2;
+            var top = 0;
+            var left = 0;
+            var bottom = canvas.Height - 1;
+            var right = canvas.Width - 1;
 
             while (!cancellationToken.IsCancellationRequested)
             {
-                Console.WriteLine("Tick...");
                 canvas.Clear();
                 var currentTime = DateTime.Now.ToLongTimeString(); 
-                canvas.DrawText(font, x, y, color, currentTime);
+                canvas.DrawText(font, x, y, textColor, currentTime);
+                canvas.DrawLine(left, top, right, top, bgColor);
+                canvas.DrawLine(right, top, right, bottom, bgColor);
+                canvas.DrawLine(left, top, left, bottom, bgColor);
+                canvas.DrawLine(left, bottom, right, bottom, bgColor);
                 matrix.SwapOnVsync(canvas);
                 await Task.Delay(TimeSpan.FromMilliseconds(500), cancellationToken);
             }
